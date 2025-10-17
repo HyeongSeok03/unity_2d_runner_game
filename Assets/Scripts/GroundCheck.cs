3f33cfd68d897bded0be0fb17d4183338846acb9
강형석 <fluor.kang@gmail.com>
2025-10-17 16:08:11 +0900

    commit1

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    public bool isGround { get; private set; }
+
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,25 +6,69 @@ public class Player : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float jumpSpeed;
     Vector2 inputVec;
+    WallCheck wc;
     Rigidbody2D rb;
-    bool isJumped;
+    SpriteRenderer sr;
+    Animator anim;
+    bool canDoubleJump = false;
+    bool isGround = false;
+
     void Awake()
     {
+        wc = GetComponentInChildren<WallCheck>();
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
-    void Update()
+    void FixedUpdate()
     {
-        var nextVec = inputVec * Time.deltaTime * speed;
-        transform.Translate(nextVec);
+        rb.linearVelocity = new Vector2(inputVec.x * speed, rb.linearVelocity.y);
     }
 
     void OnMove(InputValue value)
     {
         inputVec = value.Get<Vector2>();
+        sr.flipX = (inputVec.x != 0) ? inputVec.x < 0 : sr.flipX;
+        anim.SetBool("isWalking", inputVec.x != 0);
     }
 
     void OnJump(InputValue value)
     {
-        isJumped = true;
+        if (value.isPressed)
+        {
+            if (isGround)
+            {
+                anim.SetTrigger("jump");
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpSpeed);
+            }
+            else if (wc.isWall)
+            {
+                rb.AddForce(new Vector2(-jumpSpeed, jumpSpeed), ForceMode2D.Impulse);
+            }
+            else if (canDoubleJump)
+            {
+                anim.SetTrigger("doubleJump");
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpSpeed);
+                canDoubleJump = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Terrain"))
+        {
+            isGround = true;
+            anim.SetBool("isGround", true);
+            canDoubleJump = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Terrain"))
+        { 
+            isGround = false;
+            anim.SetBool("isGround", false);
+        }
     }
 }
